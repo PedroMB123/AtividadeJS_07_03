3633e40a35c0636772ac227b155f7aa64a9a0c4f
03$ <61505525+PedroMB123@users.noreply.github.com>
2025-03-07 09:13:04 -0300

    Update ObterHoraAtual.js

--- ArquivosDeAtividade/ObterHoraAtual.js
+++ ArquivosDeAtividade/ObterHoraAtual.js
@@ -2,9 +2,17 @@
 let data = new Date();
 
 function obterHoraAtual() {
-  if (data.getSeconds() < 10) {
+  if (data.getSeconds() < 10 && data.getMinutes() < 10) {
+    console.log(
+      `${data.getHours()}:0${data.getMinutes()}:0${data.getSeconds()}`
+    );
+  } else if (data.getSeconds() < 10) {
     console.log(
       `${data.getHours()}:${data.getMinutes()}:0${data.getSeconds()}`
+    );
+  } else if (data.getMinutes() < 10) {
+    console.log(
+      `${data.getHours()}:0${data.getMinutes()}:${data.getSeconds()}`
     );
   } else {
     console.log(`${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`);
